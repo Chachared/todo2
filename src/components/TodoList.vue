87c70c1c20bc3345bf22a8228d2097c7a6bd3aef
Charlotte Redier <62300614+Chachared@users.noreply.github.com>
2023-01-05 17:04:25 +0100

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="mb-5">TodoList Vue3 / Pinia / Axios</h1>
+        <div class="border rounded-pill text-center my-3">FILTRES</div>
+        <div class="border rounded-pill text-center my-3">INPUT</div>
+        <div class="border rounded-pill text-center my-3">LISTE</div>
+        
+    </div>
+</template>
+
+<style scoped>
+</style>
